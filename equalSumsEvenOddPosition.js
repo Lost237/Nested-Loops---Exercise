c4ba5977b56067fcda82f204ca9f9da5daf20de5
Valeria Velchovska <101146706+Lost237@users.noreply.github.com>
2022-06-05 02:11:37 +0300

    Add files via upload

---
+++ equalSumsEvenOddPosition.js
@@ -0,0 +1,18 @@
+function equalSumsEvenOddPosition(input) {
+    let start = Number(input[0]);
+    let end = Number(input[1]);
+    let num = 0;
+    let evenSum = 0;
+    let oddSum = 0;
+    for (let i = start; i <= end; i++) {
+        let currentNum = i.toString();
+        for (let index = 0; index < currentNum.length; index++) {
+            
+            if(index % 2 == 0){
+                evenSum += Number()
+            }
+        }
+    }
+}
+equalSumsEvenOddPosition(["100000",
+"100050"])
